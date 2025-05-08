89cddefd838c295aa4ac2343ccf1b1a71e904414
xatron3 <xatron3@gmail.com>
2025-05-08 22:05:19 +0200

    Mysql implementation

--- prisma/migrations/20250505134012_add_annotations/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Annotation" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "label" TEXT NOT NULL,
-    "points" JSONB NOT NULL,
-    "imageId" TEXT NOT NULL,
-    CONSTRAINT "Annotation_imageId_fkey" FOREIGN KEY ("imageId") REFERENCES "Image" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
